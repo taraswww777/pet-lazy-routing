fb28559480e20dda835dae4646b21dde9f740f90
Ковалёв Тарас Викторович (4067694) <kovalevtv@vtb.ru>
2023-06-14 13:39:00 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css'
+import React from "react";
 import {FirstPage, SecondPage} from "./pages";
+import {
+  BrowserRouter,
+  Route,
+  Link, Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <FirstPage />
-      <SecondPage />
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to={'/'}>index</Link></li>
+            <li><Link to={'/FirstPage'}>FirstPage</Link></li>
+            <li><Link to={'/SecondPage'}>SecondPage</Link></li>
+          </ul>
+          <div>
+            <Routes>
+              <Route path='/' element={'index'} />
+              <Route path='/FirstPage' element={<FirstPage />} />
+              <Route path='/SecondPage' element={<SecondPage />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </React.StrictMode>
   )
 }
 
